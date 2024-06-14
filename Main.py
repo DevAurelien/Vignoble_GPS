472a48f36447e28e9922d74f99d982aa737ffcb2
AdminArchon <lhuillier.aurelien47@gmail.com>
2024-06-14 20:52:31 +0200

    6

--- Main.py
+++ Main.py
@@ -3,78 +3,85 @@ from pathlib import Path
 from PIL import Image, ImageTk
 from tkintermapview import TkinterMapView
 
-DO_VINOS = {
-    "Alicante": ((38.3436365, -0.4881708), "Tinto"),
-    "Calatayud": ((41.3527628, -1.6422977), "Tinto"),
-    "Cariñena": ((41.3382122, -1.2263149), "Tinto"),
-    "Condado de Huelva": ((37.3382055, -6.5384658), "Blanco"),
-    "Jumilla": ((38.4735408, -1.3285417), "Tinto"),
-    "La Gomera": ((28.116, -17.248), "Blanco"),
-    "Málaga": ((36.7213028, -4.4216366), "Blanco"),
-    "Rías Baixas": ((42.459627886165265, -8.722862824636783), "Blanco"),
-    "Ribera del Duero": ((41.49232, -3.005), "Tinto"),
-    "Rioja": ((42.29993373411561, -2.486288477690506), "Tinto"),
-    "Rueda": ((41.4129785, -4.9597533), "Blanco"),
-    "Somontano": ((42.0883878, 0.0994041), "Tinto"),
-    "Tarragona": ((41.1172364, 1.2546057), "Tinto"),
-    "Txakoli de Getaria": ((43.29428414467608, -2.202397625912913), "Blanco"),
-    "Xérès": ((36.6816936, -6.1377402), "Blanco")
-}
-
 taille_icone = 25
 p = Path()
 fen = tk.Tk()
 
 import tkinter as tk
-fen = tk.Tk()
 
-class View():
-    def __init__(self, h_w_size):
-        self.geometry = fen.geometry(f"{h_w_size}x{h_w_size}")
-        # self.title = fen.title("Vinos Ibericos")
-        # self.resizable(False, False)
+
+# fen = tk.Tk()
+
+
+class Modele:
+    def __init__(self):
+        self.DO_VINOS = {
+            "Alicante": ((38.3436365, -0.4881708), "Tinto"),
+            "Calatayud": ((41.3527628, -1.6422977), "Tinto"),
+            "Cariñena": ((41.3382122, -1.2263149), "Tinto"),
+            "Condado de Huelva": ((37.3382055, -6.5384658), "Blanco"),
+            "Jumilla": ((38.4735408, -1.3285417), "Tinto"),
+            "La Gomera": ((28.116, -17.248), "Blanco"),
+            "Málaga": ((36.7213028, -4.4216366), "Blanco"),
+            "Rías Baixas": ((42.459627886165265, -8.722862824636783), "Blanco"),
+            "Ribera del Duero": ((41.49232, -3.005), "Tinto"),
+            "Rioja": ((42.29993373411561, -2.486288477690506), "Tinto"),
+            "Rueda": ((41.4129785, -4.9597533), "Blanco"),
+            "Somontano": ((42.0883878, 0.0994041), "Tinto"),
+            "Tarragona": ((41.1172364, 1.2546057), "Tinto"),
+            "Txakoli de Getaria": ((43.29428414467608, -2.202397625912913), "Blanco"),
+            "Xérès": ((36.6816936, -6.1377402), "Blanco")
+        }
+
+
+class View:
+    def __init__(self, h_w_size=800):
+        self.fen = tk.Tk()
+        self.fen.geometry(f"{str(h_w_size)}x{str(h_w_size)}")
+        self.fen.title("Vinos Ibericos")
+        self.fen.resizable(False, False)
         self.taille = str(h_w_size)
         self.taille_icone = 25
+        self.label1 = tk.Label(fen, image=ImageTk.PhotoImage(Image.open(p / "images" / "th.jpg")))
+        self.blanco = ImageTk.PhotoImage(
+            Image.open(p / "images" / "blanco.ico").resize((taille_icone, taille_icone)))
+        self.tinto = ImageTk.PhotoImage(
+            Image.open(p / "images" / "tinto.ico").resize((taille_icone, taille_icone)))
+        self.frame1 = tk.Frame(fen, height=600, width=600, highlightbackground="red", highlightthickness=5)
+        self.carte = TkinterMapView(self.frame1, width=600, height=600, corner_radius=0, bg_color="purple", max_zoom=7)
+        self.tri_des_vins()
+        self.position_depart()
+        self.bouton1 = tk.Button(fen, text="Vin")  # , command=pass)
 
     def affichage(self):
-        self.geometry(f"{self.taille}x{self.taille}")
-        self.mainloop()
+        self.label1.place(x=0, y=0)
+        self.frame1.pack()
+        self.carte.pack(fill="both")
+        self.bouton1.pack()
+        self.fen.mainloop()
 
-ecran = View(800)
+    def tri_des_vins(self):
+        for key, values in Modele().DO_VINOS.items():
+            icone_a_changer = self.tinto if values[1] == "Tinto" else self.blanco
+            self.carte.set_marker(values[0][0], values[0][1], icon=icone_a_changer)  # repeat as long as wines
+
+    def position_depart(self):
+        self.carte.set_position(41.6084332, -1.9271726, marker=False)  # Point of view
+        self.carte.set_zoom(7)  # zoom of pov
+
+
+class Controlleur:
+    def __init__(self):
+        self.modele = Modele()
+        self.vue = View()
+
+    def centrer_vue(self, nom_vin, carte_position):
+        self.vue.carte.set_position(carte_position[0], carte_position[1], marker=False)
+        self.vue.carte.set_zoom(7)
+
 
 if __name__ == "__main__":
-    ecran.affichage()
+    control = Controlleur()
+    control.vue.affichage()
 
-
-blanco = ImageTk.PhotoImage(
-    Image.open(p / "images" / "blanco.ico").resize((taille_icone, taille_icone)))
-tinto = ImageTk.PhotoImage(
-    Image.open(p / "images" / "tinto.ico").resize((taille_icone, taille_icone)))
-bg = ImageTk.PhotoImage(
-    Image.open(p / "images" / "th.jpg"))
-
-label1 = tk.Label(fen, image=bg)
-label1.place(x=0, y=0)
-frame1 = tk.Frame(fen, height=600, width=600, highlightbackground="red", highlightthickness=5)
-frame1.pack()
-carte = TkinterMapView(frame1, width=600, height=600, corner_radius=0, bg_color="purple", max_zoom=7)
-carte.pack(fill="both")
-bouton1 = tk.Button(fen, text="Vin")  # , command=pass)
-bouton1.pack()
-
-for key, values in DO_VINOS.items():
-    icone_a_changer = tinto if values[1] == "Tinto" else blanco
-    carte.set_marker(values[0][0], values[0][1], icon=icone_a_changer)  # repeat as long as wines
-
-carte.set_position(41.6084332, -1.9271726, marker=False)  # Point of view
-carte.set_zoom(7)  # zoom of pov
-
-fen.geometry("800x800")
-fen.title("Vinos Ibericos")
-fen.resizable(False, False)
-fen.mainloop()
-
-
-def centrer_vue(nom_vin, carte_position):
-    carte.set_position(carte_position[0], carte_position[1], marker=False)
-    carte.set_zoom(7)
+# fen.mainloop()
