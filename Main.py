8878e8f84969220b1e264f23a1bcfa785096e2ce
AdminArchon <lhuillier.aurelien47@gmail.com>
2024-06-17 23:43:43 +0200

    10

--- Main.py
+++ Main.py
@@ -45,7 +45,7 @@ class View:
         self.carte = TkinterMapView(self.frame1, width=600, height=600, corner_radius=0, bg_color="purple", max_zoom=7)
         self.frame_button = tk.Frame(self.fen, height=600, width=100, bd=0, highlightthickness=0, bg="#83a6a2")
         self.liste_boutons = []
-        self.create_wines_button()
+
         self.carte.delete_all_marker()
         self.start_position()
         # self.bouton1 = tk.Button(self.fen, text="Vin")  # , command=pass)
@@ -61,28 +61,32 @@ class View:
 
         self.fen.mainloop()
 
-    def create_wines_button(self):
-        for key, values in Modele().DO_VINOS.items():
-            self.liste_boutons.append(
-                tk.Button(self.frame_button, text=f"{key}", command=self.place_marker(values)))
 
-    def place_marker(self, valeur):
-        icone_a_changer = self.tinto if valeur[1] == "Tinto" else self.blanco
-        self.carte.set_marker(valeur[0][0], valeur[0][1], icon=icone_a_changer)
 
     def start_position(self):
         self.carte.set_position(41.6084332, -1.9271726, marker=False)  # Point of view
         self.carte.set_zoom(7)  # zoom of pov
 
-
 class Controlleur:
     def __init__(self):
         self.modele = Modele()
         self.vue = View()
+        self.create_wines_button()
 
     def center_view(self, nom_vin, carte_position):
         self.vue.carte.set_position(carte_position[0], carte_position[1], marker=False)
         self.vue.carte.set_zoom(7)
+
+    def create_wines_button(self):
+        for key, values in Modele().DO_VINOS.items():
+            self.vue.liste_boutons.append(
+                tk.Button(self.vue.frame_button, text=f"{key}", command=self.place_marker(values)))
+    def place_marker(self, valeur):
+        icone_a_changer = self.vue.tinto if valeur[1] == "Tinto" else self.vue.blanco
+        self.vue.carte.set_marker(valeur[0][0], valeur[0][1], icon=icone_a_changer)
+
+
+
 
 
 if __name__ == "__main__":
