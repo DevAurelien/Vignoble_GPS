caac09ed6be9255875e78ba483410aec9fe2dc42
AdminArchon <lhuillier.aurelien47@gmail.com>
2024-06-19 00:44:08 +0200

    13

--- Main.py
+++ Main.py
@@ -46,10 +46,13 @@ class View:
         self.carte = TkinterMapView(self.frame1, width=600, height=600, corner_radius=0, bg_color="purple", max_zoom=7)
         self.frame_button = tk.Frame(self.fen, height=600, width=100, bd=0, highlightthickness=0, bg="#83a6a2")
         self.liste_boutons = []
-        self.control.create_wines_button()
-        self.carte.delete_all_marker()
+        self.create_wines_button()
         self.start_position()
-        # self.bouton1 = tk.Button(self.fen, text="Vin")  # , command=pass)
+
+    def create_wines_button(self):
+        for key, values in Modele().DO_VINOS.items():
+            self.liste_boutons.append(
+                tk.Button(self.frame_button, text=f"{key}", command=lambda v=values: self.control.place_marker(v)))
 
     def affichage(self):
         self.label1.place(x=0, y=0)
@@ -58,33 +61,28 @@ class View:
         self.frame_button.pack(side="left")
         self.frame1.pack(side="left")
         self.carte.pack(fill="both")
-        # self.bouton1.pack()
-
         self.fen.mainloop()
 
     def start_position(self):
-        self.carte.set_position(41.6084332, -1.9271726, marker=False)  # Point of view
-        self.carte.set_zoom(7)  # zoom of pov
+        self.carte.set_position(40.4329067, -3.7068316, marker=False)  # Point of view
+        self.carte.set_zoom(6)  # zoom of pov
 
 
 class Controlleur:
     def __init__(self, fen):
+        self.vue = View(fen, self)
         self.fen = fen
         self.modele = Modele()
-        self.vue = View(fen, self)
 
     def center_view(self, carte_position):
         self.vue.carte.set_position(carte_position[0], carte_position[1], marker=False)
-        self.vue.carte.set_zoom(7)
+        self.vue.carte.set_zoom(6)
 
     def place_marker(self, valeur):
+        self.vue.carte.delete_all_marker()
         icone_a_changer = self.vue.tinto if valeur[1] == "Tinto" else self.vue.blanco
         self.vue.carte.set_marker(valeur[0][0], valeur[0][1], icon=icone_a_changer)
-
-    def create_wines_button(self):
-        for key, values in Modele().DO_VINOS.items():
-            self.vue.liste_boutons.append(
-                tk.Button(self.vue.frame_button, text=f"{key}", command=self.place_marker(values)))
+        self.center_view([valeur[0][0], valeur[0][1]])
 
 
 def main():
